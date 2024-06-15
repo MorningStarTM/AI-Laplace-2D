93f94b511c84c3a7440cc0a2d2800f3d7b7691f0
ErnestBeckham <ernestbeckham1998@gmail.com>
2024-06-15 11:25:56 +0530

    car files updated

---
+++ environment/track_v1/car.py
@@ -0,0 +1,52 @@
+import pygame
+import math
+import random
+from utils import blit_rotate_center
+
+
+class AbstractCar:
+    """
+    Base class for Car
+    """
+    def __init__(self, max_vel, rotation_vel):
+        self.img = pygame.image.load("assets\\BlueStrip_1.png")
+        self.max_vel = max_vel
+        self.vel = 0
+        self.rotation_vel = rotation_vel
+        self.angle = 0
+        self.x, self.y = (250, 300)
+        self.acceration = 0.1
+
+    def rotate(self, left=False, right=False):
+        if left:
+            self.angle += self.rotation_vel
+        elif right:
+            self.angle -= self.rotation_vel
+
+
+    def draw(self, win):
+        blit_rotate_center(win, self.img, (self.x, self.y), self.angle)
+
+
+    def move_forward(self):
+        self.vel = min(self.vel + self.acceration, self.max_vel)
+        self.move()
+
+    def move_backward(self):
+        self.vel = min(self.vel - self.acceration, self.max_vel/2)
+        self.move()
+
+
+    def  move(self):
+        radians = math.radians(self.angle)
+        vertical = math.cos(radians) * self.vel
+        horizontal = math.sin(radians) * self.vel
+
+        self.y -= vertical
+        self.x -= horizontal
+
+    def reduce_speed(self):
+        self.vel = max(self.vel - self.acceration / 2, 0)
+        self.move()
+       
+    
