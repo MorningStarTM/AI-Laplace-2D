3bf35f0bdb75cd37f42d61041f1fd32a1c0f954d
ErnestBeckham <ernestbeckham1998@gmail.com>
2024-07-13 23:01:04 +0530

    lidar module updated

--- environment/track_v1/LiDAR.py
+++ environment/track_v1/LiDAR.py
@@ -1,19 +1,23 @@
 import pygame
 import math
 
-
 class LiDAR:
-    def __init__(self, car_position, car_angle, num_rays=36, max_range=200, visualize=False):
-        self.car_position = car_position
-        self.car_angle = car_angle
+    def __init__(self, car_position, acceleration, max_vel, car_angle, num_rays=36, max_range=200, visualize=False):
+        self.car_position = (car_position[0]+30, car_position[1]+20)
+        self.car_angle = 1.57  # Set the initial angle
         self.num_rays = num_rays
         self.max_range = max_range
         self.visualize = visualize
+        self.acceleration = acceleration
+        self.max_vel = max_vel
+        self.vel = 0
+        
 
     def cast_rays(self, obstacles):
+        # Adjust angles to be relative to the car's forward direction
         ray_angles = [self.car_angle + math.radians(angle) for angle in range(-90, 91, 180 // (self.num_rays - 1))]
-
         distances = []
+        
         for angle in ray_angles:
             for distance in range(self.max_range):
                 end_x = self.car_position[0] + distance * math.cos(angle)
@@ -38,6 +42,7 @@ class LiDAR:
             # Draw LiDAR rays (optional for visualization)
             ray_angles = [self.car_angle + math.radians(angle) for angle in range(-90, 91, 180 // (self.num_rays - 1))]
             lidar_distances = self.cast_rays(obstacles)
+            print(lidar_distances)
 
             for i, distance in enumerate(lidar_distances):
                 angle = self.car_angle + math.radians(-90 + i * (180 // (self.num_rays - 1)))
