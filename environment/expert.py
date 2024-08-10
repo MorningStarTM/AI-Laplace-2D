d38cf2ae9c19bb79e914914d5c16ee788e24e2a8
ErnestBeckham <ernestbeckham1998@gmail.com>
2024-08-10 22:53:45 +0530

    expert demo env created

---
+++ environment/expert.py
@@ -0,0 +1,55 @@
+from gym import spaces
+import pygame
+import gym
+import numpy as np
+from track_v1 import AbstractCar, ComputerCar
+from track_v1 import RaceTrack
+import sys
+
+COLORS = {
+    'WHITE': (255, 255, 255),
+    'BLACK': (0, 0, 0),
+    'GRAY': (128, 128, 128),
+    'GREEN': (0, 255, 0)
+}
+
+WIDTH, HEIGHT = 1250, 800
+OUTER_TRACK_WIDTH = 1000
+OUTER_TRACK_HEIGHT = 700
+TRACK_THICKNESS = 220
+
+
+class CarRaceEnv(gym.Env):
+    """
+    Custom Environment for Car Racing with manual control and data collection.
+    """
+    metadata = {'render.modes': ['human']}
+
+    def __init__(self):
+        super(CarRaceEnv, self).__init__()
+
+        # Define action and observation space
+        self.action_space = spaces.Discrete(5)  # 0: No action, 1: Rotate left, 2: Rotate right, 3: Move forward, 4: Move backward
+
+        # Observation space is a vector with position (x, y), angle, and speed
+        self.observation_space = spaces.Box(low=np.array([-np.inf, -np.inf, 0, 0, -np.inf, -np.inf, -np.inf, -np.inf, -np.inf, -np.inf, -np.inf, -np.inf, -np.inf, -np.inf, -np.inf]),
+                                            high=np.array([np.inf, np.inf, 360, 10, np.inf, np.inf, np.inf, np.inf, np.inf, np.inf, np.inf, np.inf, np.inf, np.inf, np.inf]),
+                                            dtype=np.float32)
+
+        # Create RaceTrack instance
+        self.track = RaceTrack(WIDTH, HEIGHT, OUTER_TRACK_WIDTH, OUTER_TRACK_HEIGHT, TRACK_THICKNESS, COLORS)
+
+        # Initialize Pygame and create the environment
+        pygame.init()
+        self.width, self.height = 1250, 800
+        self.screen = pygame.display.set_mode((self.width, self.height))
+        pygame.display.set_caption('Car Racing Environment')
+
+        # Create Car instances
+        self.car = AbstractCar(img_path="track_v1\\assets\\BlueStrip_1.png", max_vel=5, rotation_vel=4)
+        self.car.x, self.car.y = 250, 290
+
+        self.clock = pygame.time.Clock()
+
+        # Initialize data collection
+        self.data = []
